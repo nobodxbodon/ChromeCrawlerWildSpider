c967f308bb52e2c8529f2a74419e1882d969f0f1
fromwheretowhere <fromwheretowhere.service@gmail.com>
2016-06-12 15:29:46 -0700

    demo crawler w/o UI. max speed ~3 pages/sec, tested from 10 to 80 tabs

---
+++ content.js
@@ -0,0 +1,231 @@
+// MIT License
+
+function preprocess(links) {
+  var processed = [];
+  for (var i=0; i<links.length; i++) {
+    var link = links[i];
+    var title = link.title;
+    var url = link.url;
+    if (title) {
+      title = title.trim();
+    }
+    // add document.documentURI if url is relative
+    var currentUri = document.documentURI;
+    var domain = document.location.hostname;
+    var domainUri = currentUri.substring(0, currentUri.indexOf(domain) + domain.length);
+    if (url) {
+      // 绝对url
+      if (url.indexOf('http://') === 0 || url.indexOf('https://') === 0) {
+      // 不处理url
+      }
+      // 相对domain
+      else if (url.indexOf('/') == 0) {
+        url = domainUri + url;
+      }
+      // 相对当前url
+      else {
+        url = currentUri + url;
+      }
+    }
+    processed.push({title: title, url: url})
+  };
+  return processed;
+}
+
+function isValid(link) {
+  var linkUri = link.url;
+
+  if (
+    linkUri == null || linkUri == ''
+    // exclude the bookmark on the same page
+    || getUriFromBookmark(linkUri) == getUriFromBookmark(document.documentURI)
+    // exclude link with empty title
+    || link.title == null || link.title == ''
+    // exclude javascript resource
+    || linkUri.indexOf('javascript:')>-1) {
+    
+    return false;
+  }
+  return true;
+}
+
+// get root page from bookmark url
+function getUriFromBookmark(url) {
+  var index = url.indexOf('#');
+  return index > 0
+    ? url.substring(0, index)
+    : url;
+}
+
+/* Node format:
+ attribs: object
+ children: Array
+ next: object
+ parent: object
+ prev: object
+ type: string
+ */
+function getLinksInDomNode(node) {
+  var links = [];
+  if (node.type === 'tag') {
+    if (node.name !== 'a') {
+      Array.prototype.push.apply(links, getAllLinksInDomTree(node.children));
+    } else {
+      link = extractLink(node);
+      //console.log(link);
+      links.push(link);
+    }
+  }
+  return links;
+}
+
+function extractLink(node) {
+  //console.log(node.text);
+  var children = node.children;
+  if (children.length == 1 && children[0].type === 'text') {
+    return {title: children[0].data, url: node.attribs.href};
+  } else {
+    // if children has 'span' or 'img' tag, extract text from them
+    return {title: tryExtractTextFromChildren(children), url: node.attribs.href}; 
+  }
+}
+
+function tryExtractTextFromChild(node) {
+  if (node.type === 'text') {
+    return node.data;
+  } else if (node.name === 'span' && node.type === 'tag') {
+    // get the text child
+    if (node.children.length === 1) {
+      var child = node.children[0];
+      return tryExtractTextFromChild(child);
+    }
+  } else if (node.name === 'img' && node.type === 'tag') {
+    return node.attribs.alt;
+  } else if (node.name === 'strong' && node.type === 'tag') {
+    var innerHTML = '<strong>' + tryExtractTextFromChildren(node.children) + '</strong>';
+    return innerHTML;
+  }
+  return null;
+}
+
+function tryExtractTextFromChildren(nodes) {
+  var text = "";
+  for(var i = 0; i < nodes.length; i++) {
+    var childText = tryExtractTextFromChild(nodes[i]);
+    if (childText) {
+      text += childText;
+    }
+  }
+  return text;
+}
+
+/* dom tree sample:
+ [{
+    data: 'Xyz ',
+    type: 'text'
+}, {
+    type: 'script',
+    name: 'script',
+    attribs: {
+        language: 'javascript'
+    },
+    children: [{
+        data: 'var foo = \'<bar>\';<',
+        type: 'text'
+    }]
+}, {
+    data: '<!-- Waah! -- ',
+    type: 'comment'
+}]
+https://github.com/fb55/domhandler
+*/
+// recursively walk through dom tree to get all link
+function getAllLinksInDomTree(dom) {
+  var links = [];
+  for(var i = 0; i < dom.length; i++) {
+    Array.prototype.push.apply(links, getLinksInDomNode(dom[i]));
+  }
+  return links;
+}
+
+var htmlparser = require("htmlparser2");
+
+function getAllLinksOnPage(htmlContent) {
+  var links = [];
+  
+  // TODO: 是否可以用parser解决(类似text)
+  var handler = new htmlparser.DomHandler(function (error, dom) {
+    if (error)
+      console.log(error);
+    else {
+      links = getAllLinksInDomTree(dom);
+      links = preprocess(links);
+      links = links.filter(isValid);
+    }
+  });
+  
+  var parser = new htmlparser.Parser(handler);
+  parser.write(htmlContent);
+  parser.end();
+  
+  return links;
+}
+
+function 提取文本(htmlContent) {
+  var 忽略内容TAG = ['script', 'style', 'noscript'];
+  var 文本 = [];
+  var 为文本 = true;
+  
+  var textParser = new htmlparser.Parser({
+    ontext: function(text){
+      // 删除空白文本
+      if (为文本 && text && text.trim() !== '') {
+        文本.push(text);
+      }
+    },
+    onopentag: function(tag名, attribs){
+      // TODO: no checking type of script - && (attribs.type === "text/javascript" || (attribs.language && attribs.language.toLowerCase() === 'javascript'))
+      if(忽略内容TAG.indexOf(tag名) !== -1){
+        为文本 = false;
+      }
+    },
+    onclosetag: function(tag名){
+      if(忽略内容TAG.indexOf(tag名) !== -1){
+        为文本 = true;
+      }
+    }
+  }, {decodeEntities: true});
+  
+  textParser.write(htmlContent);
+  textParser.end();
+
+  return 文本.join(' ');
+}
+
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+  console.log(msg);
+  var t = new Date();
+  
+  var htmlContent = document.documentElement.innerHTML;
+  console.log((new Date() - t) + " ms" + ' get inner html');
+  t = new Date();
+  
+  var linksOnPage = getAllLinksOnPage(htmlContent);
+  // document.getElementsByTagName('a') would save a bit time, but doesn't improve page/sec much
+  console.log((new Date() - t) + " ms" + ' get alllinks');
+  t = new Date();
+  
+  chrome.runtime.sendMessage({
+    links: linksOnPage,
+    tabId: msg.tabId,
+    extraction:
+      {
+        title: document.title,
+        url: document.URL,
+        content: 提取文本(htmlContent)
+        // change to document.documentElement.innerText also doesn't improve page/sec much
+      }
+  });
+  
+  console.log((new Date() - t) + " ms" + " End of extracting tab: " + msg.tabId + " with " + linksOnPage.length + " urls on page");
+});
---
+++ eventPage.js
@@ -0,0 +1,141 @@
+
+// 允许使用多tab并行crawl, 类似thread pool
+var 最多url数 = 100;
+var tab总数 = 3;
+
+var 待访问链接 = [];
+var 已访问链接 = [];
+var url集合 = {};
+
+var 当前tab = 0;
+// 管理tab的空闲/繁忙状态
+var 使用tab = {};
+var 当前时间 = new Date();
+var 第一个tabId;
+var 第一个tab加载开始;
+
+var db = new Dexie('WildSpider');
+
+var 重置计数 = function() {
+  待访问链接 = [];
+  已访问链接 = [];
+  url集合 = {};
+  
+  当前tab = 0;
+  使用tab = {};
+  
+  当前时间 = new Date();
+}
+
+// 链接表为需要打开的链接列表, 链接格式为{title, url};
+// 若tabId为空,直接用所有可用的新tab打开
+var 载入链接 = function(链接表, tabId) {
+  if (链接表) {
+    for (var i = 0; i < 链接表.length; i++) {
+      if (!url集合[链接表[i].url]) {
+        待访问链接.push(链接表[i]);
+        url集合[链接表[i].url] = true;
+      }
+    }
+  }
+  if (!待访问链接 || 待访问链接.length == 0) {
+    return;
+  }
+
+  if (tabId) {
+    var 下一个链接 = 待访问链接.splice(0, 1)[0].url;
+    if (已访问链接.length < 最多url数) {
+      if (tabId == 第一个tabId) {
+        console.log((new Date() - 第一个tab加载开始) + ' ms to analyze');
+        第一个tab加载开始 = new Date();
+      }
+      chrome.tabs.update(
+        tabId,
+        {
+          url: 下一个链接,
+          selected: false
+        },
+        function(tab) {
+        }
+      );
+      已访问链接.push(下一个链接);
+    }
+  }
+
+  // 如果不到tab总数, 每个新url将在新tab中打开
+  while(当前tab < tab总数) {
+    if (!待访问链接 || 待访问链接.length == 0) {
+      break;
+    }
+    var 下一链接 = 待访问链接.splice(0, 1)[0].url;
+    if (已访问链接.length < 最多url数) {
+      chrome.tabs.create({
+        url: 下一链接,
+        selected: false
+      }, function(tab) {
+        if (!第一个tabId) {
+          第一个tab加载开始 = new Date();
+          第一个tabId = tab.id;
+        }
+        使用tab[tab.id] = true;
+      });
+      已访问链接.push(下一链接);
+    }
+    当前tab++;
+  }
+}
+
+// Define a schema
+db.version(1)
+	.stores({
+		places: '++id, url, title, content, last_visit_date'
+});
+
+// Add hooks that will index "title" for full-text search:
+db.places.hook("creating", function (primKey, obj, trans) {
+  obj.last_visit_date = Date.now();
+});
+
+// Open the database
+db.open().catch(function(error){
+		alert('Uh oh : ' + error);
+});
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+  重置计数();
+  
+  var 当前Url = tab.url;
+  载入链接([{title: tab.titie, url: 当前Url}], null);
+});
+
+// 确保只在页面完全加载时触发一次
+// TODO: 页面可能卡在某个资源上不能加载完成. 添加重试与超时,允许跳过
+// 超时思路: update/create时开始定时,若onComplete不重置,则关掉tab
+chrome.webNavigation.onCompleted.addListener(function(details) {
+  if (使用tab[details.tabId]) {
+    chrome.tabs.get(details.tabId, function(tab) {
+      if(tab.url === details.url) {
+        if (details.tabId == 第一个tabId) {
+          console.log((new Date() - 第一个tab加载开始) + ' ms to load');
+          第一个tab加载开始 = new Date();
+        }
+        // 确认tab是本扩展打开的. TODO: 如果用户在这个tab打开新url,也将会被解析. 是否合理?
+        chrome.tabs.sendMessage(details.tabId, {tabId: details.tabId});
+        //console.log('onComplete in tab: ' + details.tabId + ' url: ' + details.url);
+      }
+    });
+  }
+});
+
+chrome.runtime.onMessage.addListener(function (request, _, sendResponse) {
+  // To view background log, go to chrome://extensions/ and click on that inspect view under the extension.
+  var 提取链接 = request.links;
+  var tabId = request.tabId;
+  var 提取内容 = request.extraction;
+
+  //db.places.add(提取内容);
+  
+  console.log((new Date() - 当前时间)/1000 + " sec" + " to crawl " + 已访问链接.length + ' pages');// 待访问链接数:' + 待访问链接.length);//提取内容.title + ' url:' + 提取内容.url + ' 长度: ' + 提取内容.content.length);
+  
+  载入链接(提取链接, tabId);
+});
